81ac13e49051357db19a6c5abddb47708c955a66
Horis <821938089@qq.com>
2022-11-15 16:27:46 +0800

    update

--- OulevodProvider/build.gradle.kts
+++ OulevodProvider/build.gradle.kts
@@ -4,21 +4,22 @@ version = 1
 
 cloudstream {
     // All of these properties are optional, you can safely remove them
+    language = "zh"
 
-    description = "Lorem Ipsum"
-    authors = listOf("Cloudburst")
+//    description = "Lorem Ipsum"
+//    authors = listOf("Cloudburst")
 
     /**
-    * Status int as the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta only
-    * */
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
     status = 1 // will be 3 if unspecified
 
     // List of video source types. Users are able to filter for extensions in a given category.
     // You can find a list of avaliable types here:
     // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
-    tvTypes = listOf("Others")
+    tvTypes = listOf("Movie", "AnimeMovie", "TvSeries", "Anime", "AsianDrama", "Others")
 }
