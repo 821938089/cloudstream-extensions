30c26c804fd4316f75f0652833ad33223135c382
Horis <821938089@qq.com>
2022-11-23 18:42:28 +0800

    update

--- UAPIProvider/src/main/kotlin/com/horis/cloudstreamplugins/providers/HaiwaikanProvider.kt
+++ UAPIProvider/src/main/kotlin/com/horis/cloudstreamplugins/providers/HaiwaikanProvider.kt
@@ -5,5 +5,6 @@ class HaiwaikanProvider : UAPIProvider() {
 
     override var mainUrl = "http://api.haiwaikan.com/v1/vod"
     override var name = "海外看影视"
+    override val playFromFilter = hashSetOf("haiwaikan")
 
 }
