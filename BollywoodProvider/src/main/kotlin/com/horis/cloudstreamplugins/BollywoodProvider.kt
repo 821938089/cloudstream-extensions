4ce90322f486788a2df6f2e6d9cbe818ea2ebf4d
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2022-12-14 20:09:08 +0530

    Update BollywoodProvider.kt

--- BollywoodProvider/src/main/kotlin/com/horis/cloudstreamplugins/BollywoodProvider.kt
+++ BollywoodProvider/src/main/kotlin/com/horis/cloudstreamplugins/BollywoodProvider.kt
@@ -14,11 +14,7 @@ import java.util.concurrent.ConcurrentHashMap
 class BollywoodProvider : MainAPI() {
     override val supportedTypes = setOf(
         TvType.Movie,
-        TvType.AnimeMovie,
         TvType.TvSeries,
-        TvType.Anime,
-        TvType.AsianDrama,
-        TvType.Others
     )
     override var lang = "en"
 
