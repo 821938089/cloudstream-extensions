fe1522ec5ad993c172739cd85f5bc1f36606c30b
Horis <821938089@qq.com>
2022-12-18 15:45:37 +0800

    update

--- BollywoodProvider/src/main/kotlin/com/horis/cloudstreamplugins/BollywoodPlugin.kt
+++ BollywoodProvider/src/main/kotlin/com/horis/cloudstreamplugins/BollywoodPlugin.kt
@@ -10,7 +10,7 @@ class BollywoodPlugin : Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(BollywoodProvider())
-        loadCryptoJs()
+//        loadCryptoJs()
     }
 
     private fun loadCryptoJs() {
