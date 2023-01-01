de489bda38a794655d54adabea57d5dc7522fbb2
Horis <821938089@qq.com>
2023-01-01 21:38:34 +0800

    update

---
+++ NivodProvider/src/main/kotlin/com/horis/cloudstreamplugins/NivodPlugin.kt
@@ -0,0 +1,14 @@
+package com.horis.cloudstreamplugins
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class NivodPlugin : Plugin() {
+
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(NivodProvider())
+    }
+}
