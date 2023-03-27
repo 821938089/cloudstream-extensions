a9f83a45907e6f42156d5b6d322eab5c3c60a61a
Horis <821938089@qq.com>
2023-03-27 16:41:50 +0800

    update

---
+++ HollywoodProvider/build.gradle.kts
@@ -0,0 +1,28 @@
+// use an integer for version numbers
+version = 1
+
+
+cloudstream {
+    language = "en"
+    // All of these properties are optional, you can safely remove them
+
+//    description = "Lorem Ipsum"
+    authors = listOf("Horis")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+    )
+
+    iconUrl = "https://cdn.jsdelivr.net/npm/@googledrive/index@2.0.20/images/favicon.ico"
+
+//    requiresResources = true
+}
